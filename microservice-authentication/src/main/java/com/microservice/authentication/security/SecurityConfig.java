7207d75b0b928a83062493ca44ecd19b1ecd76ca
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-06-21 20:49:03 -0400

    Cambios para en controlador para solucionar problema 2

--- microservice-authentication/src/main/java/com/microservice/authentication/security/SecurityConfig.java
+++ microservice-authentication/src/main/java/com/microservice/authentication/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login", "/auth/register").permitAll()
+                        .requestMatchers("/api/auth/login", "/api/auth/register").permitAll()
                         .requestMatchers("/auth/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
