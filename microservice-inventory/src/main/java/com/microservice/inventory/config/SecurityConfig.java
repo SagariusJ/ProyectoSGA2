ad0a52a4254df30a41ddd4d8f3b81781944317cf
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-07-13 19:33:05 -0400

    Se agregan filtros de seguridad para el servicio de inventario

--- microservice-inventory/src/main/java/com/microservice/inventory/config/SecurityConfig.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.*;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -21,6 +21,13 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        // Permite GET públicos sin token
+                        .requestMatchers("/api/inventory/all", "/api/inventory/search/**").permitAll()
+                        .requestMatchers("/api/products/all", "/api/products/search/**").permitAll()
+                        .requestMatchers("/api/stockware/all", "/api/stockware/search/**").permitAll()
+                        .requestMatchers("/api/warehouse/all", "/api/warehouse/search/**").permitAll()
+
+                        // Para cualquier otro endpoint, se requiere autenticación
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
