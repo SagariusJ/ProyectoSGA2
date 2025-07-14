b473a051b4f8cbed017d6fa6b91e9a4cc36473d0
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-07-13 20:56:30 -0400

    Se agregan filtros de seguridad para el servicio de inventario 3

--- microservice-inventory/src/main/java/com/microservice/inventory/config/SecurityConfig.java
+++ microservice-inventory/src/main/java/com/microservice/inventory/config/SecurityConfig.java
@@ -8,10 +8,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableMethodSecurity
@@ -24,19 +22,30 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> {}) // activar CORS con bean corsFilter
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        // Permite GET públicos sin token
                         .requestMatchers("/api/inventory/all", "/api/inventory/search/**").permitAll()
                         .requestMatchers("/api/products/all", "/api/products/search/**").permitAll()
                         .requestMatchers("/api/stockware/all", "/api/stockware/search/**").permitAll()
                         .requestMatchers("/api/warehouse/all", "/api/warehouse/search/**").permitAll()
-
-                        // Para cualquier otro endpoint, se requiere autenticación
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-                .cors(cors -> {}) // Habilita CORS con la configuración del bean corsConfigurationSource
                 .build();
     }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://proyectosgafront-production.up.railway.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
+
