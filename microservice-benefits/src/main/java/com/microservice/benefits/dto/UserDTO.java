d8662d35f2528f71dc11a4a6973f823a333f2028
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-07-19 01:56:29 -0400

    Pruebas para solucionar feign 3

--- microservice-benefits/src/main/java/com/microservice/benefits/dto/UserDTO.java
+++ microservice-benefits/src/main/java/com/microservice/benefits/dto/UserDTO.java
@@ -6,10 +6,6 @@ import lombok.Data;
 @Data
 public class UserDTO {
     private Long id;
+    private String username;
     private String fullName;
-    private String birthDate;
-    private String region;
-    private String commune;
-    private String address;
-
 }
