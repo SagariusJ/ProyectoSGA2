da581631cfaa44510dfb34d7fe6a6c3b8922e260
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-07-19 01:53:45 -0400

    Pruebas para solucionar feign 2

--- microservice-benefits/src/main/java/com/microservice/benefits/client/UserClient.java
+++ microservice-benefits/src/main/java/com/microservice/benefits/client/UserClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.microservice.benefits.dto.UserDTO;
 
-@FeignClient(name = "microservice-authentication")
+@FeignClient(name = "microservice-authentication", url= "https://microservice-authentication-production.up.railway.app")
 public interface UserClient {
     @GetMapping("/api/auth/public/user/{userId}")
     UserDTO getUserById(@PathVariable("userId") Long userId);
