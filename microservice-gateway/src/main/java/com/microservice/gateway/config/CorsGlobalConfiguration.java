d91918484f3129eaa150b1b09d1c8c2b0f9e9e8e
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-07-20 19:57:43 -0400

    Cambios en gateway para probar funcionamiento

--- microservice-gateway/src/main/java/com/microservice/gateway/config/CorsGlobalConfiguration.java
+++ microservice-gateway/src/main/java/com/microservice/gateway/config/CorsGlobalConfiguration.java
@@ -1,11 +1,15 @@
 package com.microservice.gateway.config;
 
 
+import com.sun.tools.javac.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.server.WebFilter;
 
 @Configuration
 public class CorsGlobalConfiguration {
@@ -15,7 +19,7 @@ public class CorsGlobalConfiguration {
         CorsConfiguration corsConfig = new CorsConfiguration();
 
         // Orígenes permitidos — aquí colocas el URL de tu frontend Angular
-        corsConfig.addAllowedOrigin("https://proyectosgafront-production.up.railway.app");
+        corsConfig.setAllowedOrigins(List.of("https://proyectosgafront-production.up.railway.app"));
 
         // Métodos permitidos (GET, POST, PUT, DELETE, etc.)
         corsConfig.addAllowedMethod("*");
@@ -31,4 +35,19 @@ public class CorsGlobalConfiguration {
 
         return new CorsWebFilter(source);
     }
+    @Bean
+    public WebFilter corsHeadersFilter() {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            response.getHeaders().add("Access-Control-Allow-Origin", "https://proyectosgafront-production.up.railway.app");
+            response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.getHeaders().add("Access-Control-Allow-Headers", "Authorization, Content-Type");
+            response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+
+            return chain.filter(exchange);
+        };
+    }
+
 }
