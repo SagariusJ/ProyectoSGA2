c65f74aa4bbecce5503f3d7a9234646bc1f2f321
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-07-20 20:00:23 -0400

    Cambios en gateway para probar funcionamiento

--- microservice-gateway/src/main/java/com/microservice/gateway/config/CorsGlobalConfiguration.java
+++ microservice-gateway/src/main/java/com/microservice/gateway/config/CorsGlobalConfiguration.java
@@ -4,12 +4,9 @@ package com.microservice.gateway.config;
 import com.sun.tools.javac.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.server.WebFilter;
 
 @Configuration
 public class CorsGlobalConfiguration {
@@ -34,20 +31,6 @@ public class CorsGlobalConfiguration {
         source.registerCorsConfiguration("/**", corsConfig);
 
         return new CorsWebFilter(source);
-    }
-    @Bean
-    public WebFilter corsHeadersFilter() {
-        return (exchange, chain) -> {
-            ServerHttpRequest request = exchange.getRequest();
-            ServerHttpResponse response = exchange.getResponse();
-
-            response.getHeaders().add("Access-Control-Allow-Origin", "https://proyectosgafront-production.up.railway.app");
-            response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-            response.getHeaders().add("Access-Control-Allow-Headers", "Authorization, Content-Type");
-            response.getHeaders().add("Access-Control-Allow-Credentials", "true");
-
-            return chain.filter(exchange);
-        };
     }
 
 }
