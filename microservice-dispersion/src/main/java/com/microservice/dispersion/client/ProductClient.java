b0b93be31f9dea7dc2c0880a1c3210333eb4a0dc
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-07-20 22:20:00 -0400

    cambios en url de feignclient

--- microservice-dispersion/src/main/java/com/microservice/dispersion/client/ProductClient.java
+++ microservice-dispersion/src/main/java/com/microservice/dispersion/client/ProductClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "products-service", url = "https://microservice-products-production.up.railway.app") // cambia el URL si usas Eureka
+@FeignClient(name = "products-service", url = "https://microservice-inventory-production.up.railway.app") // cambia el URL si usas Eureka
 public interface ProductClient {
     @GetMapping("/api/products/search/{id}")
     ProductsDTO getProductById(@PathVariable("id") Long id);
