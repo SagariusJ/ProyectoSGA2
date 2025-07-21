6603ce0daec8729ee051cb1406666f5ccbc3f323
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-07-20 21:22:04 -0400

    Cambios a feign client, por error de tipeo en url

--- microservice-informe/src/main/java/com/microservice/informe/client/CompraClient.java
+++ microservice-informe/src/main/java/com/microservice/informe/client/CompraClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
-@FeignClient(name="compra-service", url="https://microservice-authentication-production.up.railway.app")
+@FeignClient(name="compra-service", url="https://microservice-sale-production.up.railway.app")
 public interface CompraClient {
     @GetMapping("/api/compra/all")
     List<CompraDTO> getAll();
--- microservice-informe/src/main/java/com/microservice/informe/client/VentaClient.java
+++ microservice-informe/src/main/java/com/microservice/informe/client/VentaClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
-@FeignClient(name="venta-service", url="https://microservice-authentication-production.up.railway.app")
+@FeignClient(name="venta-service", url="https://microservice-sale-production.up.railway.app")
 public interface VentaClient {
     @GetMapping("/api/venta/all")
     List<VentaDTO> getAll();
