0b44f078f2af8163baff466631eba47f1588ca7f
Sagakichi <sebastian.jouannet2101@alumnos.ubiobio.cl>
2025-07-20 22:24:50 -0400

    cambios en url de feignclient

--- microservice-dispersion/src/main/java/com/microservice/dispersion/dto/ProductsDTO.java
+++ microservice-dispersion/src/main/java/com/microservice/dispersion/dto/ProductsDTO.java
@@ -5,7 +5,6 @@ import lombok.Data;
 @Data
 public class ProductsDTO {
     private Long id;
-    private String nombre;
-    private String descripcion;
-    // otros campos que tenga tu producto
+    private String product_name;
+    private String description;
 }
